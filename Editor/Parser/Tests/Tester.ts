181545b2d3d4553cda04c5f769017e24df308f89
end5 <16655171+end5@users.noreply.github.com>
2019-12-14 19:29:01 -0800

    Reorganize test cases

--- Editor/Parser/Tests/Tester.ts
+++ Editor/Parser/Tests/Tester.ts
@@ -4,19 +4,33 @@ import { lex } from "../Lexer";
 import { Parser } from "../Parser";
 import { TextRange } from "../TextRange";
 
-export function test(name: string, text: string, obj: Record<string, any>, result: string | string[], codeResult: string) {
+export function test(
+    name: string,
+    input: {
+        text: string, obj: Record<string, any>
+    },
+    output: {
+        result: string | string[], ranges: TextRange[], code: string
+    }
+) {
 
-    const lexResult = lex(text, true);
+    const lexResult = lex(input.text, true);
 
-    const parser = new Parser(lexResult.map((state) => state.token), text);
+    const parser = new Parser(lexResult.map((state) => state.token), input.text);
     const parserResult = parser.parse();
-    const interpreter = new Interpreter(parserResult.root, obj);
+    const interpreter = new Interpreter(parserResult.root, input.obj);
     const interpretResult = interpreter.interpret();
     const parserText = interpretResult.result;
     // console.log(JSON.stringify(interpretResult.stack));
     // const codeText = parserResult.root.toCode();
 
-    const resultMatch = typeof result === 'string' ? parserText === result : result.includes(parserText);
+    const resultMatch = typeof output.result === 'string' ? parserText === output.result : output.result.includes(parserText);
+    const posMatch = interpretResult.positions.every((pos, idx) =>
+        pos.start.line === output.ranges[idx].start.line &&
+        pos.start.col === output.ranges[idx].start.col &&
+        pos.end.line === output.ranges[idx].end.line &&
+        pos.end.col === output.ranges[idx].end.col
+    );
 
     // if (resultMatch && codeText === codeResult)
     if (resultMatch)
@@ -24,6 +38,8 @@ export function test(name: string, text: string, obj: Record<string, any>, resul
     else {
         if (!resultMatch)
             console.log('-- ' + name + ' ... Failed: Result did not match');
+        else if (!posMatch)
+            console.log('-- ' + name + ' ... Failed: Positions did not match');
         else
             console.log('-- ' + name + ' ... Failed: Code did not match');
         // console.log(JSON.stringify(interpretResult.stack));
@@ -39,6 +55,8 @@ export function test(name: string, text: string, obj: Record<string, any>, resul
 
         console.log('| -- Result');
         console.log('| ' + parserText);
+        console.log('| -- Positions');
+        console.log('| ' + interpretResult.positions);
         // console.log('| -- Code');
         // console.log('| ' + codeText);
 
--- Editor/Parser/Tests/Tests.ts
+++ Editor/Parser/Tests/Tests.ts
@@ -2,163 +2,295 @@ import { ConditionBuilder } from "../ConditionBuilder";
 import { test } from "./Tester";
 
 test('Retrieve string',
-    '[name]',
-    { name: 'Test' },
-    'Test',
-    'name'
+    {
+        text: '[name]',
+        obj: { name: 'Test' },
+    },
+    {
+        result: 'Test',
+        code: 'name',
+        ranges: []
+    }
 );
 
 test('Retrieve number',
-    '[str]',
-    { str: 100 },
-    '100',
-    'str'
+    {
+        text: '[str]',
+        obj: { str: 100 },
+    },
+    {
+        result: '100',
+        code: 'str',
+        ranges: []
+    }
 );
 
 test('Retrieve function',
-    '[isRed]',
-    { isRed: () => 'red' },
-    'red',
-    'isRed()'
+    {
+        text: '[isRed]',
+        obj: { isRed: () => 'red' },
+    },
+    {
+        result: 'red',
+        code: 'isRed()',
+        ranges: []
+    }
 );
 
 test('Access string',
-    '[pc.name]',
-    { pc: { name: 'Test' } },
-    'Test',
-    'pc.name'
+    {
+        text: '[pc.name]',
+        obj: { pc: { name: 'Test' } },
+    },
+    {
+        result: 'Test',
+        code: 'pc.name',
+        ranges: []
+    }
 );
 
 test('Access number',
-    '[pc.str]',
-    { pc: { str: 100 } },
-    '100',
-    'pc.str'
+    {
+        text: '[pc.str]',
+        obj: { pc: { str: 100 } },
+    },
+    {
+        result: '100',
+        code: 'pc.str',
+        ranges: []
+    }
 );
 
 test('Access function',
-    '[pc.isRed]',
-    { pc: { isRed: () => 'red' } },
-    'red',
-    'pc.isRed()'
+    {
+        text: '[pc.isRed]',
+        obj: { pc: { isRed: () => 'red' } },
+    },
+    {
+        result: 'red',
+        code: 'pc.isRed()',
+        ranges: []
+    }
 );
 
 test('Function - Create',
-    '[isRed]',
-    { isRed: () => 'red' },
-    'red',
-    'isRed()'
+    {
+        text: '[isRed]',
+        obj: { isRed: () => 'red' },
+    },
+    {
+        result: 'red',
+        code: 'isRed()',
+        ranges: []
+    }
 );
 
 test('Function - Create + Args',
-    '[isRed 0]',
-    { isRed: (args: any[]) => 'red' + args[0] },
-    'red0',
-    'isRed(0)'
+    {
+        text: '[isRed 0]',
+        obj: { isRed: (args: any[]) => 'red' + args[0] },
+    },
+    {
+        result: 'red0',
+        code: 'isRed(0)',
+        ranges: []
+    }
 );
 
 test('Function - Selector[0]',
-    '[isRed|red|blue]',
-    { isRed: (args: any[], res: string[]) => ({ selector: 0 }) },
-    'red',
-    'isRed("red", "blue")'
+    {
+        text: '[isRed|red|blue]',
+        obj: { isRed: (args: any[], res: string[]) => ({ selector: 0 }) },
+    },
+    {
+        result: 'red',
+        code: 'isRed("red", "blue")',
+        ranges: []
+    }
 );
 
 test('Function - Selector[1]',
-    '[isRed|red|blue]',
-    { isRed: (args: any[], res: string[]) => ({ selector: 1 }) },
-    'blue',
-    'isRed("red", "blue")'
+    {
+        text: '[isRed|red|blue]',
+        obj: { isRed: (args: any[], res: string[]) => ({ selector: 1 }) },
+    },
+    {
+        result: 'blue',
+        code: 'isRed("red", "blue")',
+        ranges: []
+    }
 );
 
 test('Function - Match[0]',
-    '[color red|red|blue]',
-    { color: (args: any[], res: string[]) => ({ selector: res.findIndex((r) => r === args[0]) }) },
-    'red',
-    'color(["red"], ["red", "blue"])'
+    {
+        text: '[color red|red|blue]',
+        obj: { color: (args: any[], res: string[]) => ({ selector: res.findIndex((r) => r === args[0]) }) },
+    },
+    {
+        result: 'red',
+        code: 'color(["red"], ["red", "blue"])',
+        ranges: []
+    }
 );
 
 test('Function - Match[1]',
-    '[color blue|red|blue]',
-    { color: (args: any[], res: string[]) => ({ selector: res.findIndex((r) => r === args[0]) }) },
-    'blue',
-    'color(["blue"], ["red", "blue"])'
+    {
+        text: '[color blue|red|blue]',
+        obj: { color: (args: any[], res: string[]) => ({ selector: res.findIndex((r) => r === args[0]) }) },
+    },
+    {
+        result: 'blue',
+        code: 'color(["blue"], ["red", "blue"])',
+        ranges: []
+    }
 );
 
 test('Boolean - true',
-    '[color|red|blue]',
-    { color: true },
-    'red',
-    '(color ? "red" : "blue")'
+    {
+        text: '[color|red|blue]',
+        obj: { color: true },
+    },
+    {
+        result: 'red',
+        code: '(color ? "red" : "blue")',
+        ranges: []
+    }
 );
 
 test('Boolean - false',
-    '[color|red|blue]',
-    { color: false },
-    'blue',
-    '(color ? "red" : "blue")'
+    {
+        text: '[color|red|blue]',
+        obj: { color: false },
+    },
+    {
+        result: 'blue',
+        code: '(color ? "red" : "blue")',
+        ranges: []
+    }
 );
 
 test('Concat',
-    'Hi! My name is [pc.name]. How are you?',
-    { pc: { name: 'Thomas' } },
-    'Hi! My name is Thomas. How are you?',
-    '"Hi! My name is " + pc.name + ". How are you?"'
+    {
+        text: 'Hi! My name is [pc.name]. How are you?',
+        obj: { pc: { name: 'Thomas' } },
+    },
+    {
+        result: 'Hi! My name is Thomas. How are you?',
+        code: '"Hi! My name is " + pc.name + ". How are you?"',
+        ranges: []
+    }
 );
 
 test('Code - Quotes',
-    '"Hi! My name is [pc.name]. How are you?"',
-    { pc: { name: 'Thomas' } },
-    '"Hi! My name is Thomas. How are you?"',
-    '"\\"Hi! My name is " + pc.name + ". How are you?\\""'
+    {
+        text: '"Hi! My name is [pc.name]. How are you?"',
+        obj: { pc: { name: 'Thomas' } },
+    },
+    {
+        result: '"Hi! My name is Thomas. How are you?"',
+        code: '"\\"Hi! My name is " + pc.name + ". How are you?\\""',
+        ranges: []
+    }
 );
 
 test('Code - Newline',
-    `This
+    {
+        text: `This
 is
 a
 new
 
 line`,
-    {},
-    `This
+        obj: {},
+    },
+    {
+        result: `This
 is
 a
 new
 
 line`,
-    `"This\\nis\\na\\nnew\\n\\nline"`
+        code: `"This\\nis\\na\\nnew\\n\\nline"`,
+        ranges: []
+    }
 );
 
 test('Code - Test',
-    '[name Thomas Justin Bert|THOMAS|JUSTIN|BERT|NOPE]',
     {
-        name: (args: any[], res: string[]) => ({ selector: args.findIndex((n) => n == 'Thomas') }),
-        name__toCode: (identity: string, args: string[], results: string[]) => {
-            const builder = new ConditionBuilder();
-            for (let idx = 0; idx < args.length; idx++)
-                builder.if('name == ' + args[idx], results[idx]);
-            builder.else(results[results.length - 1]);
-            return builder.build();
-        }
+        text: '[name Thomas Justin Bert|THOMAS|JUSTIN|BERT|NOPE]',
+        obj: {
+            name: (args: any[], res: string[]) => ({ selector: args.findIndex((n) => n == 'Thomas') }),
+            name__toCode: (identity: string, args: string[], results: string[]) => {
+                const builder = new ConditionBuilder();
+                for (let idx = 0; idx < args.length; idx++)
+                    builder.if('name == ' + args[idx], results[idx]);
+                builder.else(results[results.length - 1]);
+                return builder.build();
+            }
+        },
     },
-    'THOMAS',
-    '(name == "Thomas" ? "THOMAS" : (name == "Justin" ? "JUSTIN" : (name == "Bert" ? "BERT" : "NOPE")'
+    {
+        result: 'THOMAS',
+        code: '(name == "Thomas" ? "THOMAS" : (name == "Justin" ? "JUSTIN" : (name == "Bert" ? "BERT" : "NOPE")',
+        ranges: []
+    }
 );
 
 test('Super',
-    `[store.open|"Welcome to [store.name]. My name is [person.name].
-Is there anything I can help you with today?"|The sign says closed.]`,
     {
-        store: {
-            open: true,
-            name: 'TipTop'
+        text: `[store.open|"Welcome to [store.name]. My name is [person.name].
+Is there anything I can help you with today?"|The sign says closed.]`,
+        obj: {
+            store: {
+                open: true,
+                name: 'TipTop'
+            },
+            person: {
+                name: 'Thomas'
+            }
         },
-        person: {
-            name: 'Thomas'
-        }
     },
-    `"Welcome to TipTop. My name is Thomas.
+    {
+        result: `"Welcome to TipTop. My name is Thomas.
 Is there anything I can help you with today?"`,
-    `(store.open ? "\\"Welcome to " + store.name + ". My name is " + person.name + ".\\nIs there anything I can help you with today?\\"" : "The sign says closed.")`
+        code: `(store.open ? "\\"Welcome to " + store.name + ". My name is " + person.name + ".\\nIs there anything I can help you with today?\\"" : "The sign says closed.")`,
+        ranges: []
+    }
+);
+
+test('CoC2 parser code',
+    {
+        text: `[rand|
+Cait is sitting out by the fire, alternating between hugging her knees and warming her hands. She's humming a slow tune that tugs at your heartstrings though you're pretty sure you've never heard it before. Is this what music sounds like in Jassira?
+|
+Cait's sitting by the fire, but looks up at you as you pass by the fire with a little smile. <i>"If you want to have a quick chat, take your mind off things, I'm always game for it. [flags.CAIT_FUCKED|Unless you'd like to take off a little stress from the road, that is...]"</i>
+|
+Cait's parked herself by the fire, sitting cross-legged with her eyes closed and her staff lengthwise across her lap. She seems to be meditating — but what <i>do</i> devotees of Mallach meditate on? Fantasising about various deliciously lewd sex acts, perhaps? Does she have ritual mental exercises to get herself in the mood at-will?
+
+Or maybe you're just overthinking things.
+]`,
+        obj: {
+            rand(args: any[], results: any[]) {
+                return { selector: Math.floor(Math.random() * results.length) };
+            },
+            flags: {
+                CAIT_FUCKED: true
+            }
+        },
+    },
+    {
+        result: [`
+Cait is sitting out by the fire, alternating between hugging her knees and warming her hands. She's humming a slow tune that tugs at your heartstrings though you're pretty sure you've never heard it before. Is this what music sounds like in Jassira?
+`,
+            `
+Cait's sitting by the fire, but looks up at you as you pass by the fire with a little smile. <i>"If you want to have a quick chat, take your mind off things, I'm always game for it. Unless you'd like to take off a little stress from the road, that is..."</i>
+`,
+            `
+Cait's parked herself by the fire, sitting cross-legged with her eyes closed and her staff lengthwise across her lap. She seems to be meditating — but what <i>do</i> devotees of Mallach meditate on? Fantasising about various deliciously lewd sex acts, perhaps? Does she have ritual mental exercises to get herself in the mood at-will?
+
+Or maybe you're just overthinking things.
+`],
+        code: 'rand("\nCait is sitting out by the fire, alternating between hugging her knees and warming her hands. She\'s humming a slow tune that tugs at your heartstrings though you\'re pretty sure you\'ve never heard it before. Is this what music sounds like in Jassira?", "\nCait\'s sitting by the fire, but looks up at you as you pass by the fire with a little smile. <i>\"If you want to have a quick chat, take your mind off things, I\'m always game for it.\" " + (flags.CAIT_FUCKED ? "Unless you\'d like to take off a little stress from the road, that is..." : "") + "</i>", "\nCait\'s parked herself by the fire, sitting cross-legged with her eyes closed and her staff lengthwise across her lap. She seems to be meditating — but what <i>do</i> devotees of Mallach meditate on? Fantasising about various deliciously lewd sex acts, perhaps? Does she have ritual mental exercises to get herself in the mood at-will?\n\nOr maybe you\'re just overthinking things.")',
+        ranges: []
+    }
 );
